5024100f8ce9ef656c8d036373083cb3dc694b94
Andsirpa <andsirpa94@gmail.com>
2024-02-06 19:23:36 +0100

    added tasks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-console.log('JS OK');
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            newTask: {
+                text: '',
+                done: false
+            },
+            tasks: [
+                {
+                    text: 'svolgere la traccia',
+                    done: true
+                },
+                {
+                    text: 'fare benzina',
+                    done: false
+                },
+                {
+                    text: 'regalo di frank',
+                    done: false
+                },
+                {
+                    text: 'reso amazon',
+                    done: false
+                },
+
+            ]
+        }
+    },
+    created() { },
+
+
+
+}).mount('#app')
