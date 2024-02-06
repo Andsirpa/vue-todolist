8b11ca7cd75c36ffdda81847b7021915fa2c76ba
Andsirpa <andsirpa94@gmail.com>
2024-02-06 19:43:15 +0100

    added methods

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ createApp({
                     done: false
                 },
                 {
-                    text: 'reso amazon',
+                    text: 'prepara la cena',
                     done: false
                 },
 
@@ -29,6 +29,31 @@ createApp({
         }
     },
     created() { },
+
+    // preparo i metodi
+    methods: {
+
+        removeTask(index) {
+            // uso lo splice per rimuovere un task in base all'indice
+            this.tasks.splice(index, 1);
+        },
+
+
+        addTask() {
+
+            if (this.newTask.text !== '') {
+                // uso unshift per aggiunger un nuovo task alla lista
+                this.tasks.unshift({ ...this.newTask });
+            }
+            this.newTask.text = '';
+        },
+
+
+        // Cambio lo stato 'done' di un todo quando il testo viene cliccato
+        toggleDone(index) {
+            this.tasks[index].done = !this.tasks[index].done;
+        }
+    }
 
 
 
